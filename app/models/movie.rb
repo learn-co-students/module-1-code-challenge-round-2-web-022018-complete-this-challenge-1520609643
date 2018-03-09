1917f3286e84d28028971e98c543c43677be6304
bafab <bafa@tutanota.de>
2018-03-09 11:05:12 -0500

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,31 @@
 class Movie
   attr_accessor :title
-
   @@all = []
 
   def initialize(title)
     @title = title
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
 
+  def watchlistings
+    WatchListing.all.select do |x| x.movie == self end
+  end
+
+  def viewers
+    watchlistings.select do |x| x.viewer end.uniq
+  end
+
+  def average_rating
+    sum = 0.0
+    watchlistings.each do |x| sum += x.rating end
+    sum / watchlistings.length
+  end
+
+  def self.highest_rated
+    @@all.sort_by do |x| x.average_rating end.last
+  end
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,15 +1,22 @@
 class Viewer
   attr_accessor :username
-
   @@all = []
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
+  def add_to_watchlist(movies)
+    movies.each do |x| WatchListing.new(self, x) end
+  end
+
+  def rate_movie(movie, rating)
+    this = WatchListing.all.find do |x| x.movie == movie && x.viewer == self end
+    this.rating = rating
+  end
 end
