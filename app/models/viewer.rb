591305ae2d6d08f417dddb78dfd86d386eb832cc
Abigail Swarth <abigailswarth@gmail.com>
2018-03-09 11:12:57 -0500

    code challenge

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,25 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def add_to_watchlist(movies)
+    movies.each {|movie| WatchListing.new(self, movie)}
+  end
+
+  def rate_movie(movie, rating)
+    watchlistings.find do |list|
+      if list.movie == movie
+        list.rating = rating
+      end
+    end
+  end
+
+  def movies
+    watchlistings.map {|list| list.movie }
+  end
+
+  def watchlistings
+    WatchListing.all.select {|list| list.viewer == self }
+  end
+
 end
